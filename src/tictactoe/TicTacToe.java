b183b14dbf07b2235ad64b7521db42064cdaace8
Rekkuj <reija.jokinen@kolumbus.fi>
2019-08-07 19:01:14 +0300

    Finished very simple Java TicTacToe game

--- src/tictactoe/TicTacToe.java
+++ src/tictactoe/TicTacToe.java
@@ -1,5 +1,7 @@
 package tictactoe;
 
+import javax.swing.JFrame;
+
 /*
 * Simple TicTacToe game
 * @author Reija J
@@ -7,6 +9,11 @@ package tictactoe;
 
 public class TicTacToe {
     public static void main(String[] args) {
-
+        JFrame ticTacToe = new TicTacToeFrame();
+        ticTacToe.setTitle("TicTacToe Game");
+        ticTacToe.setSize(600, 600);
+        ticTacToe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ticTacToe.setLocationRelativeTo(null);
+        ticTacToe.setVisible(true);
     }
 }
--- src/tictactoe/TicTacToeFrame.java
+++ src/tictactoe/TicTacToeFrame.java
@@ -13,6 +13,7 @@ import javax.swing.border.LineBorder;
 public class TicTacToeFrame extends JFrame {
     // Whose turn it is?
     private char whoseTurn = 'X';
+    private boolean gameOver = false;
 
     // Create cell grid
     private Cell[][] cells = new Cell[3][3];
@@ -46,7 +47,92 @@ public class TicTacToeFrame extends JFrame {
         return true;
     }
 
+    /*
+    * Determines if a given token has won
+    */
+    public boolean hasWon(char token) {
+        // Check rows
+        for (int i = 0; i < 3; i++) {
+            if ((cells[i][0].getToken() == token) && (cells[i][1].getToken() == token) && (cells[i][2].getToken() == token)) {
+                return true;
+            }
+        }
 
+        // Check columns
+        for (int j = 0; j < 3; j++) {
+            if ((cells[0][j].getToken() == token) && (cells[1][j].getToken() == token) && (cells[2][j].getToken() == token)) {
+                return true;
+            }
+        }
 
+        // Check diagonal
+        if ((cells[0][0].getToken() == token) && (cells[1][1].getToken() == token) && (cells[2][2].getToken() == token)) {
+                return true;
+        }
+
+        if ((cells[0][2].getToken() == token) && (cells[1][1].getToken() == token) && (cells[2][0].getToken() == token)) {
+            return true;
+        }
+
+        return false;
+    }
+    // Defines a cell in a TicTacToe game board
+    public class Cell extends JPanel {
+        // token of this cell
+        private char token = ' ';
+
+        // Constructor
+        public Cell() {
+            setBorder(new LineBorder(Color.black, 1));
+            addMouseListener(new MyMouseListener());
+        }
+
+        public char getToken() {
+            return token;
+        }
+
+        public void setToken(char c) {
+            token = c;
+            repaint();
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+
+            if (token == 'X') {
+                g.drawLine(10, 10, getWidth() - 10, getHeight() - 10);
+                g.drawLine(getWidth() - 10, 10, 10, getHeight() - 10);
+            } else if (token == 'O') {
+                g.drawOval(10, 10, getWidth() - 20, getHeight() - 20);
+            }
+        }
+
+        private class MyMouseListener extends MouseAdapter {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (gameOver) {
+                    return;
+                }
+
+                // if the cell is empty and the game is not over
+                if (token == ' ' && whoseTurn != ' ')
+                    setToken(whoseTurn);
+
+                // Check the game status
+                if (hasWon(whoseTurn)) {
+                    jlblStatus.setText(whoseTurn + " won! Game over!");
+                    whoseTurn = ' ';
+                    gameOver = true;
+                } else if (isFull()) {
+                    jlblStatus.setText("Tie game! Game over!");
+                    whoseTurn = ' ';
+                    gameOver = true;
+                } else {
+                    whoseTurn = (whoseTurn == 'X') ? 'O' : 'X';
+                    jlblStatus.setText(whoseTurn + "'s turn.");
+                }
+            }
+        }
     }
 }
